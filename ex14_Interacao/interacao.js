21d0246b4716df642bd1dfcc7521bce5fbb11c0f
Aline Bertolazo dos Santos <118446826+Aline016@users.noreply.github.com>
2023-03-04 15:25:27 -0300

    Add files via upload

---
+++ ex14_Interacao/interacao.js
@@ -0,0 +1,8 @@
+function fcnInteracao(){
+    let resultado = new Date
+
+    let saida = document.getElementById("section1")
+
+    saida.innerHTML = `<p>Foi recebido do sistema: ${resultado} </p>`
+
+}
